cb05eb4037a6e2ab7cc586a99ba8f9600c2a274d
PatrickHastings2 <pjhpersons@gmail.com>
2024-03-28 10:33:08 -0700

    carcrash

--- api/titanicapi.py
+++ api/titanicapi.py
@@ -6,37 +6,33 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 from model.titanic import dt, logreg, enc, cols
 
-# Initialize Flask
+# initialize Flask
 app = Flask(__name__)
 
-# Create Titanic API
+# create the api
 titanic_api = Blueprint('titanic_api', __name__, url_prefix='/api/titanic')
 
-# Predict survival using a POST request
+# predict survival using a post request
 @titanic_api.route('/predict', methods=['POST'])
 def predict():
     try:
-        # Get data from the request
+        # get data from the request
         data = request.get_json(force=True)
         features = pd.DataFrame(data, index=[0])
 
-        # Convert data for the model
+        # convert data for the model
         onehot = enc.transform(features[['embarked']]).toarray()
         features = features.join(pd.DataFrame(onehot, columns=cols))
         features.drop(['embarked'], axis=1, inplace=True)
 
-        # Make sure there's no missing data
-        # Replace missing values with some defaults if needed
-        # Example: features.fillna(0, inplace=True)
-
-        # Predict with models and get survival probability
+        # predict with models and get survival probability
         prediction_dt_proba = dt.predict_proba(features)
         prediction_logreg_proba = logreg.predict_proba(features)
 
         survival_probability_dt = prediction_dt_proba[0][1]
         survival_probability_logreg = prediction_logreg_proba[0][1]
 
-        # Return predictions as percentages
+        # return predictions as percentages
         return jsonify({
             'DT Probability': f"{survival_probability_dt:.2%}",
             'LogReg Probability': f"{survival_probability_logreg:.2%}"
